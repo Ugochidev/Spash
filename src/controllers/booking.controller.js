0f461aa58c746a53d97fe90d4974515951b6814d
Ugochidev <ugochukwuchioma16@gmail.com>
2022-04-12 16:45:07 +0100

    endpoint for spash shortlets

--- src/controllers/booking.controller.js
+++ src/controllers/booking.controller.js
@@ -23,7 +23,7 @@ const bookShortlets = async (req, res, next) => {
     await validatebooking.validateAsync(req.body);
 
     let totalAmount = noOfRooms * noOfNights * amountPerDay;
-    log;
+    console.log(totalAmount);
     // booking
     const newbooking = await db.query(
       "INSERT INTO booking (reservation, time, amountPerDay, noOfNights,noOfRooms,totalAmount, date, shortlets_id,) VALUES ($1, $2, $3, $4, $5, $6, $7, $8,) RETURNING *",
