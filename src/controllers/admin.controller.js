b862e6b3b93a655da240ab4bf82810c15d1c4968
Ugochidev <ugochukwuchioma16@gmail.com>
2022-04-12 06:45:41 +0100

    endpoint for spash shortlets

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -192,7 +192,7 @@ const forgetPassword = async (req, res, next) => {
     }
     await bcrypt.hash(confirmPassword, 10);
     await db.execute(
-    "UPDATE admin SET password = ? WHERE password = ?",
+    "UPDATE admin SET password = $1 WHERE password = $2",
       [ password ]
     );
     return successResMsg(res, 200, {
